e11a78008152cb0de3dbc2e4510d28b906dce205
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-08-26 11:23:45 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate crypto;
+extern crate rand;
+
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use rand::Rng;
+use std::mem;
+
+fn main() {
+    let mut hasher = Sha256::new();
+    let mut rng = rand::thread_rng();
+
+    hasher.input(b"TEST");
+    let prev_hash = hasher.result_str();
+    let transactions = [b"Give A 0.1BTC", b"Give B 1.5BTC"];
+
+    let mut nonce = rng.gen::<u64>();
+    loop {
+        hasher = Sha256::new();
+        unsafe {
+            let nonce_bytes = mem::transmute::<u64, [u8; 8]>(nonce);
+            hasher.input(&nonce_bytes);
+            hasher.input(prev_hash.as_bytes());
+            let trans_iter = transactions.into_iter();
+            for &each in trans_iter {
+                hasher.input(each);
+            }
+            let result = hasher.result_str();
+            if result.starts_with("07") {
+                println!("FOUND NONCE");
+                println!("NONCE: {}", nonce);
+                println!("HASH: {}", result);
+                break;
+            }
+        }
+        nonce += 1;
+        println!("{}", nonce);
+    }
+}

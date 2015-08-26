b1350957990b9498c59af217f50bc1c5782595ae
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-08-26 12:33:47 -0700

    improvement: Decompose Into Functions

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,44 @@ use std::mem;
 // The number of leading zeros required in hash to reach difficulty target
 const DIFFICULT_ZEROS: &'static str = "00";
 
+
+fn get_hash(nonce: u64,
+            prev_hash: &String,
+            transactions: &Vec<&'static str>) -> String {
+    let mut hasher = Sha256::new();
+
+    unsafe {
+        let nonce_bytes = mem::transmute::<u64, [u8; 8]>(nonce);
+        hasher.input(&nonce_bytes);
+    }
+    hasher.input(prev_hash.as_bytes());
+    for each in transactions.iter() {
+        hasher.input(each.as_bytes());
+    }
+
+    return hasher.result_str();
+}
+
+
+fn hits_difficulty(hash: &String) -> bool {
+    return hash.starts_with(DIFFICULT_ZEROS);
+}
+
+
 fn main() {
     let mut hasher = Sha256::new();
     hasher.input(b"TEST");
     
-    let prev_hash = hasher.result_str();
-    let transactions = [b"Give A 0.1BTC", b"Give B 1.5BTC"];
+    let prev_hash: String = hasher.result_str();
+    let transactions: Vec<&'static str> = vec!["Give A 0.1BTC", "Give B 1.5BTC"];
 
     let mut rng = rand::thread_rng();
-    let mut nonce = rng.gen::<u64>();
+    let mut nonce: u64 = rng.gen::<u64>();
 
     loop {
-        hasher = Sha256::new();
-
-        unsafe {
-            let nonce_bytes = mem::transmute::<u64, [u8; 8]>(nonce);
-            hasher.input(&nonce_bytes);
-        }
-        hasher.input(prev_hash.as_bytes());
-        let trans_iter = transactions.into_iter();
-        for &each in trans_iter {
-            hasher.input(each);
-        }
-
-        let result = hasher.result_str();
-        if result.starts_with(DIFFICULT_ZEROS) {
+        let result = get_hash(nonce, &prev_hash, &transactions);
+        
+        if hits_difficulty(&result) {
             println!("\n[Found Nonce]");
             println!("nonce: {}", nonce);
             println!("{}", result);

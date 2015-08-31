a7cbeb8a38978d0613137f1d4d1429ae98025f53
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-08-31 14:29:18 -0700

    Complete Simple Proof Of Work System

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use rand::{Rng, ThreadRng};
 use std::mem;
 use std::iter::repeat;
 
+static BITMASKS: [u16; 17] = [
+    0x0000, 0x8000, 0xC000, 0xE000, 0xF000, 0xF800, 0xFC00, 0xFE00, 0xFF00,
+    0xFF80, 0xFFC0, 0xFFE0, 0xFFF0, 0xFFF8, 0xFFFC, 0xFFFE, 0xFFFF
+];
+
 fn get_hash(nonce: u64,
             prev_hash: &String,
             transactions: &Vec<String>) -> Box<Sha256> {
@@ -24,7 +29,6 @@ fn get_hash(nonce: u64,
     return Box::new(hasher);
 }
 
-
 fn get_hasher_bytes(hasher: Box<Sha256>) -> Vec<u8> {
     let mut buf: Vec<u8> = repeat(0).take((hasher.output_bits() + 7) / 8).collect();
     let mut unboxed_hasher = *hasher;
@@ -32,15 +36,15 @@ fn get_hasher_bytes(hasher: Box<Sha256>) -> Vec<u8> {
     return buf;
 }
 
-fn hits_difficulty(hash: &mut [u8]) -> bool {
-    return (hash[0] == 0x00) && (hash[1] & 0xF0 == 0x00);
+fn hits_difficulty(bitmask: u16, hash: &mut [u8]) -> bool {
+    let upper_mask: u8 = hash[0] & (((bitmask >> 8) & 0xFF) as u8);
+    let lower_mask: u8 = hash[1] & ((bitmask & 0xFF) as u8);
+    return upper_mask == 0 && lower_mask == 0;
 }
-
 
 fn random_transaction(rng: &mut ThreadRng) -> String {
     return format!("Give B {} BTC", rng.gen::<f64>().to_string());
 }
-
 
 fn main() {
     let mut hasher = Sha256::new();
@@ -49,29 +53,33 @@ fn main() {
     let prev_hash: String = hasher.result_str();
     let mut rng = rand::thread_rng();
 
-    let mut transactions: Vec<String> = vec![
-        "Give A 0.1BTC".to_owned(), "Give B 1.5BTC".to_owned()
-            ];
-    let mut nonce: u64 = rng.gen::<u64>();
+    for &mask in BITMASKS.iter() {
+        println!("\n\nBitmask: {:x}", mask);
 
-    loop {
-        let hasher = get_hash(nonce, &prev_hash, &transactions);
-        let mut unboxed_hasher = *hasher;
-        let hash_str = unboxed_hasher.result_str();
-        let mut bytes = get_hasher_bytes(hasher);
-        
-        if hits_difficulty(&mut bytes) {
-            println!("\n[Found Nonce]");
-            println!("Nonce: {}", nonce);
-            println!("# Transactions: {}", transactions.len());
-            println!("{}", hash_str);
-            break;
+        let mut transactions: Vec<String> = vec![
+            "Give A 0.1BTC".to_owned(), "Give B 1.5BTC".to_owned()
+        ];
+        let mut nonce: u64 = rng.gen::<u64>();
+
+        loop {
+            let hasher = get_hash(nonce, &prev_hash, &transactions);
+            let mut unboxed_hasher = *hasher;
+            let hash_str = unboxed_hasher.result_str();
+            let mut bytes = get_hasher_bytes(hasher);
+            
+            if hits_difficulty(mask, &mut bytes) {
+                println!("\n[Found Nonce]");
+                println!("Nonce: {}", nonce);
+                println!("# Transactions: {}", transactions.len());
+                println!("{}", hash_str);
+                break;
+            }
+
+            if transactions.len() < 50000 {
+                transactions.extend(vec![random_transaction(&mut rng)]);
+            }
+
+            nonce += 1;
         }
-
-        if transactions.len() < 50000 {
-            transactions.extend(vec![random_transaction(&mut rng)]);
-        }
-
-        nonce += 1;
     }
 }
